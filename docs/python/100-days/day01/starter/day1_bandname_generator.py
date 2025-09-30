1b07c7e1de38740e7b6ed8db83e26be8133f9b8b
Pharns Genece <Pharns.Genece@me.com>
2025-09-30 16:54:23 -0700

    day1 project completed

---
+++ docs/python/100-days/day01/starter/day1_bandname_generator.py
@@ -0,0 +1,9 @@
+# --- Greeting --- #
+print("Welcome to the Band Name Generator.")
+
+# --- Variable --- #
+city = input("What's the name of the city you grew up in?\n")
+pet_name = input("Whats your pet's name? \n")
+
+# --- New Band Name Output ---#
+print("Your band name could be: " + city + " " + pet_name)
--- docs/python/100-days/day01/starter/task.py
+++ docs/python/100-days/day01/starter/task.py
@@ -1,2 +1,19 @@
 # Write your code below this line 👇
-print("Hello " + input("What is your name?"))
+# print(len(input("What is your name? ")))
+
+# username = input("What is your name? ")
+# length = len(username)
+# print(username)
+# print(length)
+
+
+glass1 = "milk"
+glass2 = "juice"
+
+newglass = glass1
+
+glass1 = glass2
+glass2 = newglass
+
+print(glass1)
+print(glass2)
