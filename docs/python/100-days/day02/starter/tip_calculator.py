6a9e233c650b5ea91f33bcdf4d87bbcdf4ea45ad
Pharns Genece <Pharns.Genece@me.com>
2025-10-01 12:36:10 -0700

    tip_calulator project

---
+++ docs/python/100-days/day02/starter/tip_calculator.py
@@ -0,0 +1,15 @@
+# --- Welcome ---#
+print("Welcome to the tip calculator!")
+
+# --- Inputs ---#
+total_bill = float(input("What was the total bill? "))
+tip_percentage = float(input("How much tip would you like to give? 10, 12,or15? "))
+number_people = float(input("How many people to split the bill?"))
+
+# --- Calculation ---#
+amount_due = round(
+    (total_bill + (total_bill * (tip_percentage / 100))) / number_people, 2
+)
+
+# --- Output ---#
+print(f"Each person should pay: {amount_due}")
