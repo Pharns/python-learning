cf88df748957c3e8bbfaf333c4032ac1ba6d2d09
Pharns Genece <Pharns.Genece@me.com>
2025-10-01 12:36:10 -0700

    tip_calulator project

--- docs/python/100-days/day02/starter/task.py
+++ docs/python/100-days/day02/starter/task.py
@@ -1,1 +1,34 @@
-bmi = 84 / 1.65 ** 2
+# --- len("Hello")
+
+# print(type("String"))
+# print(type(1234))
+# print(type(123.45))
+# print(type(True))
+# print(type(False))
+
+
+# print(3 * (3 + 3) / 3 - 3)
+
+# height = 1.65
+# weight = 84
+# bmi = round(weight / height**2, 2)
+
+# print(int(bmi))
+# print(bmi)
+
+
+# score = 0
+
+# # User scores a point
+# score += 1
+# print(score)
+
+
+### f-strings
+score = 0
+height = 1.8
+is_winning = True
+
+print(
+    f"Your score is {score}, your height is {height}. You are winning is {is_winning}\n"
+)
